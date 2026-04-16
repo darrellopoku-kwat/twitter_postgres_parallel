ee76966248165dcc51a232bc9583947d2414115b
Darrell Opoku Kwateng <darrellopoku@icloud.com>
2026-04-15 21:36:03 -0700

    fix load.py

--- load_tweets.py
+++ load_tweets.py
@@ -103,7 +103,6 @@ def insert_tweet(connection,tweet):
 
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
-    connection.rollback()
     with connection.begin() as trans:
 
         ########################################
