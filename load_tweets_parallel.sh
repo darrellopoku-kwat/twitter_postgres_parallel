cc141127b5e2e1d0b7a322ae981b435dce284e32
Darrell Opoku Kwateng <darrellopoku@icloud.com>
2026-04-15 23:47:06 -0700

    fix parallel

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 files=$(find data/*)
 
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_denormalized.sh
+time echo "$files" | parallel ./load_denormalized.sh {}
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:10931/postgres --inputs
+time echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:10931/postgres --inputs {}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 load_tweets_batch.py --db=postgresql://postgres:pass@localhost:10932/postgres --inputs
+time echo "$files" | parallel python3 load_tweets_batch.py --db=postgresql://postgres:pass@localhost:10932/postgres --inputs {}
